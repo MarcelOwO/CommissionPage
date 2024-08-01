55a6e89c31d1d4bac6628545b261520521c73ab5
MarcelOwO <marcel-sigl@gmx.de>
2024-08-01 21:27:25 +0200

    updating ui

--- CommissionPage/Program.cs
+++ CommissionPage/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CommissionPage;
+using Blazorise;
+using Blazorise.FluentUI2;
+using Blazorise.Icons.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorise().AddFluentUI2Providers().AddFluentUIIcons();
 
 await builder.Build().RunAsync();
