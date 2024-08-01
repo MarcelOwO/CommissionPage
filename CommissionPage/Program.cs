b2ec2b987e77d05f33b6cbbc73904420fded6c53
MarcelOwO <marcel-sigl@gmx.de>
2024-08-02 01:53:33 +0200

    recreating using mudblazor. Seems easier to use lol

--- CommissionPage/Program.cs
+++ CommissionPage/Program.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CommissionPage;
-using Blazorise;
-using Blazorise.FluentUI2;
-using Blazorise.Icons.FluentUI;
+using CommissionPage.wwwroot;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddBlazorise().AddFluentUI2Providers().AddFluentUIIcons();
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<DarkModeService>();
 
 await builder.Build().RunAsync();
---
+++ CommissionPage/wwwroot/DarkModeService.cs
@@ -0,0 +1,13 @@
+﻿namespace CommissionPage.wwwroot;
+
+public class DarkModeService
+{
+    public bool IsDarkMode { get; private set; } = false;
+    public event Action? OnDarkModeChange;
+    public void ToggleDarkMode(bool isDarkMode)
+    {
+        IsDarkMode =  isDarkMode;
+        OnDarkModeChange?.Invoke();
+    }
+    
+}
