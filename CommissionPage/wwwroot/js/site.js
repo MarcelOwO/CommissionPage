32cc2c7f9988d0e4d1e128103fb7d3fd7a030588
MarcelOwO <marcel-sigl@gmx.de>
2024-08-03 08:14:03 +0200

    UwU

--- CommissionPage/wwwroot/js/site.js
+++ CommissionPage/wwwroot/js/site.js
@@ -1,16 +1,4 @@
-﻿window.scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-window.addEventListener('scroll', () => {
-    const button = document.querySelector('.back-to-top');
-    if (window.scrollY > 200) {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
-});
-
+﻿
 
 // wwwroot/js/site.js
 window.showLoadingSpinner = () => {
